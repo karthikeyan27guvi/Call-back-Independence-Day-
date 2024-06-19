9029778db65254ff1200dd2e409ca0cfc516902b
karthikeyan27guvi <karthikeyan.github@gmail.com>
2024-06-19 20:22:39 +0530

    Independence Day

---
+++ script.js
@@ -0,0 +1,55 @@
+setTimeout(()=>{
+    console.log("Timeout 1");
+    let div = document.getElementById('root')
+    div.innerText = "10"
+setTimeout(()=>{
+    console.log("Timeout 2");
+    let div = document.getElementById('root')
+    div.innerText = "9"
+setTimeout(()=>{
+    console.log("Timeout 3");
+    let div = document.getElementById('root')
+    div.innerText = "8"
+setTimeout(()=>{
+    console.log("Timeout 4");
+    let div = document.getElementById('root')
+    div.innerText = "7"
+setTimeout(()=>{
+    console.log("Timeout 5");
+    let div = document.getElementById('root')
+    div.innerText = "6"
+setTimeout(()=>{
+    console.log("Timeout 6");
+    let div = document.getElementById('root')
+    div.innerText = "5"
+setTimeout(()=>{
+    console.log("Timeout 7");
+    let div = document.getElementById('root')
+    div.innerText = "4"
+setTimeout(()=>{
+    console.log("Timeout 8");
+    let div = document.getElementById('root')
+    div.innerText = "3"
+setTimeout(()=>{
+    console.log("Timeout 9");
+    let div = document.getElementById('root')
+    div.innerText = "2"
+setTimeout(()=>{
+    console.log("Timeout 10");
+    let div = document.getElementById('root')
+    div.innerText = "1"
+    setTimeout(()=>{
+    console.log("Timeout 11");
+    let div = document.getElementById('root')
+    div.innerText = "Happy Independence Day!!"
+    },1000)                                      
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
